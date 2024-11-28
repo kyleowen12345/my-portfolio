bb38d240c11a8a0891d177ee173cb64daed3188c
Kyle Owen Ga <kyleowenga@Kyles-MacBook-Air.local>
2024-11-28 11:10:54 +0800

    chore: scroll adjustment

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,20 @@
-import '../styles/globals.css'
-import "@fontsource/poppins/400.css"
-import "@fontsource/roboto-mono/400.css"
-import { ChakraProvider } from "@chakra-ui/react"
-import {Global,css} from '@emotion/react'
-import theme from '../styles/theme';
-import Header from '../components/header';
-import { MyRouteProvider } from '../lib/routeprovider';
-
+import "../styles/globals.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/roboto-mono/400.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import theme from "../styles/theme";
+import Header from "../components/header";
+import { MyRouteProvider } from "../lib/routeprovider";
 
 const GlobalStyle = ({ children }) => {
   return (
     <>
-
       <Global
         styles={css`
-          body{
-            background-color: #0A192F;
-            scroll-behavior: smooth;
+          body {
+            background-color: #0a192f;
+            scroll-behavior: smooth !important;
           }
         `}
       />
@@ -27,16 +25,14 @@ const GlobalStyle = ({ children }) => {
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}  resetCSS>
-         <MyRouteProvider>
-         <GlobalStyle/>
-         <Header/>
-         <Component {...pageProps} />
-         </MyRouteProvider>
-     </ChakraProvider >
-  )
-  
-
+    <ChakraProvider theme={theme} resetCSS>
+      <MyRouteProvider>
+        <GlobalStyle />
+        <Header />
+        <Component {...pageProps} />
+      </MyRouteProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
